14849fda471d6df385ce0b376c0d9dd0c17b492b
Elena Piaggio <elenapiaggio@gmail.com>
2025-03-01 15:32:32 +0100

    ADD Iteration 5

--- src/index.js
+++ src/index.js
@@ -40,9 +40,43 @@ function removeProduct(event) {
 }
 
 // ITERATION 5
-
 function createProduct() {
-  //... your code goes here
+  console.log('create product clicado');
+  const productName = document.querySelector('tfoot .create-product input[type="text"]').value;
+  const productPrice = document.querySelector('tfoot .create-product input[type="number"]').value;
+  console.log("name", productName);
+  console.log("price", productPrice);
+
+  // Creo una plantilla a partir del html de ironhack
+  const templateRow = document.querySelector('#cart tbody .product');
+
+  // Clono la template para no modificar la template original
+  // el parámetro true es para copiar también el contenido (los hijos)
+  const newRow = templateRow.cloneNode(true);
+
+  // Seteo los valores por defecto a la nueva fila clonada
+  newRow.querySelector('.quantity input').value = 0;
+  newRow.querySelector('.subtotal span').textContent = 0;
+
+  // añado al clon los valores que leo del formulario
+  newRow.querySelector('.name span').textContent = productName;
+  newRow.querySelector('.price span').textContent = productPrice;
+
+  // Añado la fila 
+  document.querySelector('#cart tbody').appendChild(newRow);
+
+  // Asignar el evento onclick al botón REMOVE
+  const removeButton = newRow.querySelector('.btn-remove');
+  removeButton.onclick = () => {
+    newRow.remove();
+    calculateAll();
+  };
+
+  // Limpiar fila después de añadir el producto
+  document.querySelector('tfoot .create-product input[type="text"]').value = '';
+  document.querySelector('tfoot .create-product input[type="number"]').value = '0';
+
+
 }
 
 window.addEventListener('load', () => {
@@ -55,5 +89,6 @@ window.addEventListener('load', () => {
     button.addEventListener('click', removeProduct);
   });
 
-
+  const createProductBtn = document.getElementById('create');
+  createProductBtn.addEventListener('click', createProduct);
 });
